d8a6eb74c579d70d9bd371954a9cde0a33a4327e
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-09-18 06:01:43 +0900

    Change: Routing for job/show action.

--- apps/frontend/modules/job/templates/indexSuccess.php
+++ apps/frontend/modules/job/templates/indexSuccess.php
@@ -4,7 +4,14 @@
       <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>">
         <td class="location"><?php echo $job->getLocation() ?></td>
         <td class="position">
-          <a href="<?php echo url_for('job/show?id='.$job->getId()) ?>">
+          <a href="<?php echo url_for(array(
+            'module'   => 'job',
+            'action'   => 'show',
+            'id'       => $job->getId(),
+            'company'  => $job->getCompany(),
+            'location' => $job->getLocation(),
+            'position' => $job->getPosition(),
+          )) ?>">
             <?php echo $job->getPosition() ?>
           </a>
         </td>
