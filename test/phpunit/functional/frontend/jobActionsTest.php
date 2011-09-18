3ed9180703bee12c3ec7878aa1113caec939d2d0
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-09-19 01:02:19 +0900

    Add: FUnctional test.

--- test/phpunit/functional/frontend/jobActionsTest.php
+++ test/phpunit/functional/frontend/jobActionsTest.php
@@ -28,4 +28,17 @@ class functional_frontend_jobActionsTest extends sfPHPUnitBaseFunctionalTestCase
             checkElement('title', '/Jobeet - Your best job board/')->
         end();
     }
+
+    /**
+     * @test
+     */
+    public function 期限切れのJobは表示されない()
+    {
+        $this->getBrowser()->
+            get('/')->
+
+        with('response')->begin()->
+            checkElement('.jobs td.position:contains("expired")', false)->
+        end();
+    }
 }
